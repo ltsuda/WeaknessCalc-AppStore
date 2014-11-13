9e383c2ede7fc70f25439c930df3a93f060ffd38
Leonardo Henrique Tsuda <ltsuda@ltsuda-mbr.LTSUDA>
2014-11-13 21:50:44 -0200

    Revert "navigationbar color set to the same background"
    This reverts commit 73ee055218397a86906a2258869d584b815797bf.

--- PokeWeakness/PokeWeakness/PokemonDetailsViewController.m
+++ PokeWeakness/PokeWeakness/PokemonDetailsViewController.m
@@ -33,11 +33,9 @@ static NSString *kSkillCellIdentifier = @"kSkillCellIdentifier";
 	// Do any additional setup after loading the view.
 
 
-
     NSString *skillName = [[PokemonSkill stringFromSkillType:self.pokemon.firstType] lowercaseString];
     self.backgroundImageView.image = [UIImage imageNamed:skillName];
-    [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed: [NSString stringWithFormat:@"%@bg",skillName]] forBarMetrics:UIBarMetricsDefault];
-    self.navigationController.navigationBar.translucent = NO;
+    
     
     //change TableViewHeader color
     [[UITableViewHeaderFooterView appearance] setTintColor:[UIColor lightTextColor]];
@@ -72,7 +70,6 @@ static NSString *kSkillCellIdentifier = @"kSkillCellIdentifier";
 - (void)viewDidAppear:(BOOL)animated
 {
     [self.skillsTableView flashScrollIndicators];
-
     
     [super viewDidAppear:animated];
 }
@@ -80,7 +77,6 @@ static NSString *kSkillCellIdentifier = @"kSkillCellIdentifier";
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-
     
     [self.navigationController setNavigationBarHidden:NO animated:YES];
 
